2df9ffbf2dfe77ced16c4a18dda6ebb730b71f06
Navaneethan <nava.nmr@gmail.com>
2018-02-08 14:58:08 +0530

    added django test

---
+++ superlists/lists/test_django.py
@@ -0,0 +1,13 @@
+
+from django.test import TestCase
+
+
+class HomePageTest(TestCase):
+
+    def test_uses_home_template(self):
+        response = self.client.get('/')
+        self.assertTemplateUsed(response, 'home.html')
+
+    def test_home_page_uses_item_form(self):
+        response = self.client.get('/')
+        self.assertEquals([], response.context['items'])
