133fd64b43fb6ba2746570625d0be8a0a8ec45cb
Navaneethan <nava.nmr@gmail.com>
2018-02-08 14:43:15 +0530

    deleted unwanted files

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # tdd
 Django TDD  Example project
+
+### Path to the geck driver for loading firefox with selenium
+
+> export PATH=$PATH:/home/navatux/workspace/tdd/gecko_binary/
+
+
--- todo/superlists/lists/utils.py
+++
@@ -1,9 +0,0 @@
-import os
-
-
-class RemovalService(object):
-    """A service for removing objects from the filesystem."""
-    
-    def rm(self, filename):
-        if os.path.isfile(filename):
-            os.remove(filename)
